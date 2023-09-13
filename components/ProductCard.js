b05c135069cbed737a91c419149ec9fbba898168
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-09-13 09:12:24 -0700

    working with productcard

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -8,10 +8,32 @@ const ProductCard = ({ image, category, title, price, description }) => {
 
   return (
     <View className="w-full bg-white rounded-3xl p-5 my-5">
-      <Image source={{ uri: image }} className="w-full h-72" />
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{price}</Text>
+      <Image
+        source={{ uri: image }}
+        className="w-full h-72 "
+        style={{ resizeMode: "contain" }}
+      />
+      <View className="mt-5">
+        <Text className="text-sm opacity-60">{category}</Text>
+
+        <Text>{title}</Text>
+        <View className="flex-row justify-between">
+          <View className="flex-row gap-4 items-center">
+            <AntDesign
+              name="minuscircleo"
+              size={24}
+              onPress={() => setCount(count - 1)}></AntDesign>
+            <Text className="text-xl ">{count}</Text>
+            <AntDesign
+              name="pluscircleo"
+              size={24}
+              onPress={() => setCount(count + 1)}></AntDesign>
+          </View>
+          <Text className="text-2xl font-extrabold">${price * count}</Text>
+        </View>
+
+        <Text>{description}</Text>
+      </View>
     </View>
   );
 };
