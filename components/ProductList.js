900cb7a1b167cea177e0008d24e6874e7ecb0f4b
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-09-16 20:23:31 -0700

    another cleaning

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import React from "react";
 import { products } from "../product";
 import ProductCard from "./ProductCard";
