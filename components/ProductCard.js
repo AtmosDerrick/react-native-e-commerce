2982684ecb8e6d0cb02a465a5606ada33be0830a
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-09-16 20:22:18 -0700

    remove color schema

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
-import { useColorScheme } from "react-native";
 
 const ProductCard = ({ image, category, title, price, description }) => {
   const [count, setCount] = useState(1);
